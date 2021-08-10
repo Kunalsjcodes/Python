510342943b103b47244ff5dbabaedd5aae42284a
Kunalsjcodes <kunal.sj01@gmail.com>
2021-08-10 12:01:49 +0530

    Operators

---
+++ Assignment Operators.py
@@ -0,0 +1,10 @@
+a=5 #assigning 5 to a
+print(a)
+a+=5 # its just like a=a+5
+print(a)
+a-=5 # its just like a=a-5
+print(a)
+a*=5 # its just like a=a*5
+print(a)
+a/=5 # its just like a=a/5
+print(a)
---
+++ Logical Operators.py
@@ -0,0 +1,7 @@
+# Logical Operators # 
+# means if something is true or not
+a=5
+b=10
+a==5 and b==10 # and means both the condition needs to be true
+a==5 or b==9 # or means either one of the condition needs to be true
+# not basically inverses the results
